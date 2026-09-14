06fd9a54ecd87381de8a7834364a3fc02ab6ceee
idah4624 <idahonuh@gmail.com>
2026-09-14 17:26:01 +0100

    updated library management api project

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from accounts.models import User
+
+
+class RegistrationTests(APITestCase):
+
+    def test_user_registration(self):
+
+        data = {
+            "username": "john",
+            "email": "john@gmail.com",
+            "password": "john12345",
+            "role": "member"
+        }
+
+        response = self.client.post(
+            "/api/accounts/register/",
+            data,
+            format="json"
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+        self.assertEqual(
+            User.objects.count(),
+            1
+        )
